cebfae151301066b24bb4ccebdfe286f47337dca
Godfrey O. <40922747+osasere1m@users.noreply.github.com>
2025-08-04 18:44:59 +0100

    Update adrs.py

--- adrs.py
+++ adrs.py
@@ -11,8 +11,8 @@ from pybit.unified_trading import HTTP
 
 
 bybit = ccxt.bybit({
-    'apiKey': 'LQLW7aAhcalaYMAiUe',
-    'secret': 'X02KF8x2VVXuXDQmoWAd8TCXx3dS7M7fAaKD',
+    'apiKey': '',
+    'secret': '',
     'enableRateLimit': True,
     'options': {
         'defaultType': 'future',
@@ -22,8 +22,8 @@ bybit = ccxt.bybit({
 
 session = HTTP(
     testnet=False,
-    api_key="LQLW7aAhcalaYMAiUe",
-    api_secret="X02KF8x2VVXuXDQmoWAd8TCXx3dS7M7fAaKD",
+    api_key="",
+    api_secret="",
 )
 #bybit.set_sandbox_mode(True) # activates testnet mode
 
